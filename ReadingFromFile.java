850e14dbc628c7414138ffa8739d5c99e5ec0de6
Aigerim Raimbay <raimbay89@gmail.com>
2024-05-27 10:35:40 -0500

    reading from a file

---
+++ ReadingFromFile.java
@@ -0,0 +1,12 @@
+package io;
+import java.io.*;
+public class ReadingFromFile {
+
+    public static void main(String[] args) throws Exception {
+        FileReader fr = new FileReader("/Users/araimbay/Personal/ACADEMY/Programming/P1/myText.txt");
+        int i;
+        while ((i=fr.read()) != -1){
+            System.out.print((char) i);
+        }
+    }
+}
